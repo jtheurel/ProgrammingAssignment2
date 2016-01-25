4a8d2fc66c0a0b033bd175cba716ee8fb1be5c93
Javier Theurel <javiertheurel@gmail.com>
2016-01-24 21:39:30 -0600

    adding final comments to the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
 ## create a special "matrix" object that can cache its inverse
 ##last submission testing SHA
+##set and get to apply to matrix
 makeCacheMatrix <- function(x = matrix()) {
 		inv <- NULL
-	set <- function(y) {
-		x <<- y
+	set <- function(y) {     
+		x <<- y  ##an enviroment variable differete from the current
 		inv <<- NULL
 	}##function(Y)
 
@@ -21,12 +22,12 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	inv <- x$getinv()
 
-	if(!is.null(inv)){
+	if(!is.null(inv)){  ##this happend when the data has been already computed
 		message("Getting Cached Data")
 		return(inv)
 	}##if
 	matr <- x$get()
-	inv <- solve(matr, ...)
+	inv <- solve(matr, ...)   ##function solve
 	x$setinv(inv)
 	return(inv)
 }##function
